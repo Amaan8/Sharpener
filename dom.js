e2355a228bbe13a184a39d17a2a2cbd8f435f2b8
Amaan8 <amaanmas1@gmail.com>
2022-11-04 20:25:01 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,6 @@
+let header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+
+let items = document.getElementById('add');
+items.style.fontWeight = 'bold';
+items.style.color = 'green';
